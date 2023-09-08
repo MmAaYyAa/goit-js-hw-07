66390f1674626c20d768e8fbce8697777fc782e3
MmAaYyAa <132580599+MmAaYyAa@users.noreply.github.com>
2023-09-08 09:29:17 +0100

    url large img

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+//console.log(galleryContainer)
+
+// <li class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </li>
+
+const markup = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item" >
+<a class="gallery__link" href="${original}">
+<img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+</a>
+</li>`
+);
+
+galleryContainer.insertAdjacentHTML("beforeend", markup.join(""));
+
+galleryContainer.addEventListener("click", onClick)
+
+function onClick(event){
+    event.preventDefault();
+    
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+//console.log(event.target);
+    const largeImg = event.target.dataset.source;
+    console.log(largeImg);
+   
+ 
+}
+ 
+
+//console.log(galleryItems);
+
